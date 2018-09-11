5f2cd2d798357e75390fd805396bf840bbf5af46
xiawm <xiawm@ldomotors.com>
2018-09-11 15:46:08 +0800

    完成了测试

--- Client/Client.py
+++ Client/Client.py
@@ -1,14 +1,53 @@
 import sys
 import requests
 import os
+import json
 
 # 屏蔽warning信息
 requests.packages.urllib3.disable_warnings()
 
+
 def download(url, file_path):
+    headers = {
+        "Host": "youpin.mi.com",
+        "Content-Type": "application/json",
+        "Referer": "http://192.168.128.29/",  # 必须带这个参数，不然会报错
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36",
+        "Range":"0-1024",
+             }
+    url = "http://localhost:18080/client"
+
     # 第一次请求是为了得到文件总大小
-    r1 = requests.get(url, stream=True, verify=False)
-    total_size = int(r1.headers['Content-Length'])
+   # data = {'getlen'}
+   # data=urllib.urlencode(values)
+   # r1 = requests.get(url, stream=True, verify=False)
+    #total_size = int(r1.headers['Content-Length'])
+
+    #登录
+    pyload = {"Head":"Login","UserID":"12345678","Password":"12345678"}
+    response = requests.post(url, data=json.dumps(pyload), headers=headers).text
+    res = json.loads(response)
+
+    #获取文件列表
+    pyload = {"Head":"GetFrameList","SessionID":res['SessionID'],"FirmwareKind":"LiftTable","FirmwareVersion":"V1"}
+    response = requests.post(url, data=json.dumps(pyload), headers=headers).text
+    res2 = json.loads(response)
+
+    #下载文件
+    pyload = {"Head":"GetFrame","SessionID":res['SessionID'],"URL":res2[2]['URL']}
+    r = requests.post(url, data=json.dumps(pyload), headers=headers)
+    filename = res2[2]['FrameName']
+
+    with open(file_path + '\\' + filename, "ab") as f:
+            for chunk in r.iter_content(chunk_size=1024):
+                if chunk:
+                    f.write(chunk)
+                    f.flush()
+
+
+    pyload = {"Head":"getleanth"}
+    response = requests.post(url, data=json.dumps(pyload), headers=headers).text
+    size = json.loads(response)["size"]
 
     # 这重要了，先看看本地文件下载了多少
     if os.path.exists(file_path):
@@ -44,7 +83,7 @@ def download(url, file_path):
 if __name__ == '__main__':
     link = r'http://localhost:18080/reg'
     UUID = r'2a4a3044-0b1a-4722-83ed-43ba5d6d25b0'
-    path = r'F:\test\Client\a.exe'
+    path = r'./DownLoad'
     url = link #os.path.join(link, UUID)
     # 调用一下函数试试
     download(url, path)
